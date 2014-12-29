09324fd0a203fc99dd7451cc6a9f981397ea8dbd
sebv <seb.vincent@gmail.com>
2014-12-29 16:34:38 +0800

    added lint and clean gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
+"use strict";
+
 var gulp = require('gulp'),
-    rename = require("gulp-rename"),
     rimraf = require('gulp-rimraf'),
-    replace = require('gulp-replace');
+    replace = require('gulp-replace'),
+    clean = require('gulp-clean'),
+    jshint = require('gulp-jshint'),
+    jscs = require('gulp-jscs');
 
 var argv = require('yargs')
   .count('notraceur')
@@ -9,20 +13,43 @@ var argv = require('yargs')
   .argv;
 
 function getPackageName() {
-  if(!argv['name']) throw new Error('Missing package name.');
-  return argv['name'];
+  if (!argv.name) throw new Error('Missing package name.');
+  return argv.name;
 }
 
-gulp.task('rimraf-package', function() {
+gulp.task('clean', function () {
+  return gulp.src(['base/node_modules','base-traceur/nodE_modules',
+      'base-traceur/build'], {read: false})
+    .pipe(clean());
+});
+
+gulp.task('jscs', function () {
+  return gulp
+   .src(['*.js'])
+   .pipe(jscs());
+});
+
+gulp.task('jshint', function () {
+  return gulp
+   .src(['*.js'])
+   .pipe(jshint())
+   .pipe(jshint.reporter('jshint-stylish'))
+   .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('lint',['jshint','jscs']);
+
+gulp.task('rimraf-package', function () {
   var packageName = getPackageName();
   return gulp.src('./out/' + packageName, {read: false})
     .pipe(rimraf());
 });
 
-gulp.task('create-package', ['rimraf-package'], function() {
+gulp.task('create-package', ['rimraf-package'], function () {
   var packageName = getPackageName();
   return gulp.src(['**/*','**/.*'], {cwd: argv.notraceur ? 'base' : 'base-traceur'})
     .pipe(replace(/new-appium-package/g, packageName))
     .pipe(gulp.dest('./out/' + packageName));
 });
 
+gulp.task('default', ['lint', 'clean']);
